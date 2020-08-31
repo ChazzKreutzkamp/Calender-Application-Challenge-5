d9caee9ae2f115f0250f022b0d12fdbbd96ef333
ChazzKreutzkamp <66965486+ChazzKreutzkamp@users.noreply.github.com>
2020-08-30 19:30:42 -0600

    accidentally copied over the html file instead of the updated logic.js file that fixed the timedBackground problem.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,104 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
+var text = [];
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
-  <link rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" />
-  <link rel="stylesheet" href="./assets/css/style.css" />
-  <title>Work Day Scheduler</title>
-</head>
+var initialize = function() {
+    var currentDayEl = document.querySelector("#currentDay");
+    var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    currentDayEl.textContent = date;
+    var timeNow = moment().format("H");
+    timeNow = parseInt(timeNow);
+    timedBackground(timeNow);
+    savedStuffInitialize();
+}
 
-<body>
-  <header class="jumbotron">
-    <h1 class="display-3">Work Day Scheduler</h1>
-    <p class="lead description">A simple calendar app for scheduling your work day</p>
-    <p id="currentDay" class="lead"></p>
-  </header>
-  <div class="container">
-    <!-- Timeblocks go here -->
-    <div class="row time-block">
-      <div class="col-2 hour">9am</div>
-      <textarea class="col-8"  id="9-hundred"></textarea>
-      <div class="col-2 d-flex">
-        <button class="saveBtn" id="9"><span class="oi oi-clipboard"></span></button>
-      </div>
-    </div>
+var savedStuffInitialize = function() {
+    text = JSON.parse(localStorage.getItem("textList"));
+    if(!text) {
+        text = [];
+        return;
+    }
+    for (var i = 0; i < text.length; i++){
+        var savedtext = text[i].textInput;
+        var savedEl = text[i].savedElement;
+        $(savedEl).text(savedtext);
+    }
+}
 
-    <div class="row time-block">
-      <div class="col-2 hour">10am</div>
-      <textarea class="col-8" id="10-hundred"></textarea>
-      <div class="col-2 d-flex">
-        <button class="saveBtn" id="10"><span class="oi oi-clipboard"></span></button>
-      </div>
-    </div>
+$(".saveBtn").on("click", function() {
+    var clickedBtn = $(this).attr("id");
+    var inputId = "#" + clickedBtn + "-hundred";
+    var toBeSavedText = $(inputId).val().trim();
+    var toBeSavedObject = {
+        textInput: toBeSavedText,
+        savedElement: inputId
+    };
+    for (var i = 0; i < text.length; i++){
+        if (text[i].savedElement === inputId){
+            text[i] = toBeSavedObject;
+            localStorage.setItem("textList", JSON.stringify(text));
+            return;
+        }
+    }
+    text.push(toBeSavedObject);
+    localStorage.setItem("textList", JSON.stringify(text));
+});
 
-    <div class="row time-block">
-      <div class="col-2 hour">11am</div>
-      <textarea class="col-8"  id="11-hundred"></textarea>
-      <div class="col-2 d-flex">
-        <button class="saveBtn" id="11"><span class="oi oi-clipboard"></span></button>
-      </div>
-    </div>
- 
-    <div class="row time-block">
-      <div class="col-2 hour">12pm</div>
-      <textarea class="col-8" id="12-hundred"></textarea>
-      <div class="col-2 d-flex">
-        <button class="saveBtn" id="12"><span class="oi oi-clipboard"></span></button>
-      </div>
-    </div>
+var timedBackground = function(timeNow) {  
+    for (var i = 9; i<=17; i++) {
+        var comparitorId = "#" + i + "-hundred";
+        if(timeNow === i) {
+            $(comparitorId).removeClass("present past future");
+            $(comparitorId).addClass("present");
+        }
+        else if (timeNow > i) {
+            $(comparitorId).removeClass("present past future");
+            $(comparitorId).addClass("past");
+        }
+        else if (timeNow < i) {
+            $(comparitorId).removeClass("present past future");
+            $(comparitorId).addClass("future");
+        }
+    }
+}
 
-    <div class="row time-block">
-      <div class="col-2 hour">1pm</div>
-      <textarea class="col-8" id="13-hundred"></textarea>
-      <div class="col-2 d-flex">
-        <button class="saveBtn" id="13"><span class="oi oi-clipboard"></span></button>
-      </div>
-    </div>
+setInterval(function(){
+    var timeNow = moment().format("H");
+    timeNow = parseInt(timeNow);
+    timedBackground(timeNow);
+  }, (1000*60));
 
-    <div class="row time-block">
-      <div class="col-2 hour">2pm</div>
-      <textarea class="col-8" id="14-hundred"></textarea>
-      <div class="col-2 d-flex">
-        <button class="saveBtn" id="14"><span class="oi oi-clipboard"></span></button>
-      </div>
-    </div>
+setInterval(function() {
+    var currentDayEl = document.querySelector("#currentDay");
+    var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    currentDayEl.textContent = date;
+},1000);
 
-    <div class="row time-block">
-      <div class="col-2 hour">3pm</div>
-      <textarea class="col-8" id="15-hundred"></textarea>
-      <div class="col-2 d-flex">
-        <button class="saveBtn" id="15"><span class="oi oi-clipboard"></span></button>
-      </div>
-    </div>
-
-    <div class="row time-block">
-      <div class="col-2 hour">4pm</div>
-      <textarea class="col-8" id="16-hundred"></textarea>
-      <div class="col-2 d-flex">
-        <button class="saveBtn" id="16"><span class="oi oi-clipboard"></span></button>
-      </div>
-    </div>
-
-    <div class="row time-block">
-      <div class="col-2 hour">5pm</div>
-      <textarea class="col-8" id="17-hundred"></textarea>
-      <div class="col-2 d-flex">
-        <button class="saveBtn" id="17"><span class="oi oi-clipboard"></span></button>
-      </div>
-    </div>
-
-  </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="./assets/js/logic.js"></script>
-</body>
-
-</html>
+  initialize();
