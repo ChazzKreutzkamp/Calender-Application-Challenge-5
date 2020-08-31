5dcc1ee44104d5209e7cd3a43e8bd87920f5c1f5
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2020-08-30 18:21:28 -0600

    Local storage now works and initializes everytime the browser is refreshed. All features are now working for the applications requirements.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,5 @@
 var text = [];
+
 var initialize = function() {
     var currentDayEl = document.querySelector("#currentDay");
     var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
@@ -6,9 +7,44 @@ var initialize = function() {
     var timeNow = moment().format("H");
     timeNow = parseInt(timeNow);
     timedBackground(timeNow);
+    savedStuffInitialize();
 }
 
+var savedStuffInitialize = function() {
+    text = JSON.parse(localStorage.getItem("textList"));
+    if(!text) {
+        text = [];
+        return;
+    }
+    for (var i = 0; i < text.length; i++){
+        var savedtext = text[i].textInput;
+        var savedEl = text[i].savedElement;
+        $(savedEl).text(savedtext);
+        console.log(i);
+    }
+}
+
+$(".saveBtn").on("click", function() {
+    var clickedBtn = $(this).attr("id");
+    var inputId = "#" + clickedBtn + "-hundred";
+    var toBeSavedText = $(inputId).val().trim();
+    var toBeSavedObject = {
+        textInput: toBeSavedText,
+        savedElement: inputId
+    };
+    for (var i = 0; i < text.length; i++){
+        if (text[i].savedElement === inputId){
+            text[i] = toBeSavedObject;
+            localStorage.setItem("textList", JSON.stringify(text));
+            return;
+        }
+    }
+    text.push(toBeSavedObject);
+    localStorage.setItem("textList", JSON.stringify(text));
+});
+
 var timedBackground = function(timeNow) {  
+    console.log(timeNow);
     for (var i = 9; i<=17; i++) {
         var comparitorId = "#" + i + "-hundred";
         if(timeNow === i) {
@@ -24,12 +60,15 @@ var timedBackground = function(timeNow) {
 }
 
 setInterval(function(){
-    var currentDayEl = document.querySelector("#currentDay");
-    var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-    currentDayEl.textContent = date;
     var timeNow = moment().format("H");
     timeNow = parseInt(timeNow);
     timedBackground(timeNow);
-  },(1000*60)*30);
+  }, (1000*60));
+
+setInterval(function() {
+    var currentDayEl = document.querySelector("#currentDay");
+    var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    currentDayEl.textContent = date;
+},1000);
 
   initialize();
